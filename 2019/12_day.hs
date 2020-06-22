155d0713093214311ec88feba13d48a5b2b29271
Michal Mrena <mrena.michal@gmail.com>
2020-06-22 14:14:33 +0200

    It is done.

--- 2019/12_day.hs
+++ 2019/12_day.hs
@@ -1,8 +1,7 @@
 import Data.Char
+import qualified Data.Set as S
 
 input = ["<x=1, y=3, z=-11>", "<x=17, y=-10, z=-8>", "<x=-1, y=-15, z=2>", "<x=12, y=-4, z=-4>"]
--- input = ["<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>", "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>"]
--- input = ["<x=-8, y=-10, z=0>", "<x=5, y=5, z=10>", "<x=2, y=-7, z=3>", "<x=9, y=-8, z=-3>"]
 
 type Positions  = [Int]
 type Velocities = [Int]
@@ -11,14 +10,15 @@ type Position   = Int
 isDigitOrSign :: Char -> Bool
 isDigitOrSign c = any ($c) [(=='+'), (=='-'), isDigit]
 
-extractNum :: String -> (Int, String)
-extractNum s = let rest = dropWhile (not . isDigitOrSign) s
-               in  (read $ takeWhile isDigitOrSign rest, dropWhile isDigitOrSign rest)
+extractInt :: String -> (Int, String)
+extractInt s = let rest = dropWhile (not . isDigitOrSign) s
+                   ints = takeWhile isDigitOrSign rest
+               in  (read $ ints, drop (length ints) rest)
 
 parsePoint :: String -> (Int, Int, Int)
-parsePoint is = let (x, rx) = extractNum is
-                    (y, ry) = extractNum rx
-                    (z, rz) = extractNum ry
+parsePoint is = let (x, rx) = extractInt is
+                    (y, ry) = extractInt rx
+                    (z, rz) = extractInt ry
                 in  (x, y, z)
 
 cmp :: (Ord a) => a -> a -> Int
@@ -39,14 +39,19 @@ simulateStep :: (Positions, Velocities) -> (Positions, Velocities)
 simulateStep (ps, vs) = let newVs = updateVelocities ps vs
                         in  (zipWith (+) ps newVs, newVs)
 
-dimensionEnergy :: (Positions, Velocities) -> Int
-dimensionEnergy (ps, vs) = (sum $ map abs ps) * (sum $ map abs vs)
+initVs :: [[Int]]
+initVs = take 3 $ repeat (take (length input) $ repeat 0)
 
 solvePart1 :: Int
-solvePart1 = let initVs = take 3 $ repeat (take (length input) $ repeat 0)
-                 result = map (last . take 1001 . iterate simulateStep) $ (`zip` initVs) $ toList $ unzip3 $ map parsePoint input
-                 ps     = map (map abs) $ map fst result
-                 vs     = map (map abs) $ map snd result
-                 pot    = zipWith3 (\ a b c -> a + b + c) (ps !! 0) (ps !! 1) (ps !! 2)
-                 kin    = zipWith3 (\ a b c -> a + b + c) (vs !! 0) (vs !! 1) (vs !! 2)
-             in  sum $ zipWith (*) pot kin
+solvePart1 = let simResult           = map (last . take 1001 . iterate simulateStep) $ (`zip` initVs) $ toList $ unzip3 $ map parsePoint input
+                 ps                  = map (map abs) $ map fst simResult
+                 vs                  = map (map abs) $ map snd simResult
+                 dimSum [xs, ys, zs] = zipWith3 (\ a b c -> a + b + c) xs ys zs
+             in  sum $ zipWith (*) (dimSum ps) (dimSum vs)
+
+firstLoopLength :: (Ord a) => S.Set a -> [a] -> Int
+firstLoopLength ss (x:xs) | x `S.member` ss = S.size ss
+                          | otherwise       = firstLoopLength (S.insert x ss) xs
+
+solvePart2 = let simResult = map (map (\ (ps, vs) -> ps ++ vs)) $ map (iterate simulateStep) $ (`zip` initVs) $ toList $ unzip3 $ map parsePoint input
+             in  foldl1 lcm $ map (firstLoopLength S.empty) $ simResult
