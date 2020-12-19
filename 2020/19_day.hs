246c0354f95e8afdcfa4179c45d236cd034087c6
Michal Mrena <mrena.michal@gmail.com>
2020-12-19 19:42:23 +0100

    Day 19.

--- 2020/19_day.hs
+++ 2020/19_day.hs
@@ -6,7 +6,7 @@ import qualified Data.IntMap as M
 type RuleId = Int
 data Rule   = Leaf Char
             | Sequence [RuleId]
-            | Alternative Rule Rule deriving Show
+            | Alternative [Rule] deriving Show
 type Rules = IntMap Rule
 
 parseInput :: String -> (Rules, [String])
@@ -18,31 +18,30 @@ parseInput input = (M.fromList . map parseRuleLine $ rulesStr, messages)
                 [i, rest] = splitOn ":" line
 
         parseRule str | '"' `elem` str = Leaf (str !! 1)
-                      | '|' `elem` str = (\[l, r] -> Alternative (parseRule l) (parseRule r)) . splitOn "|" $ str
+                      | '|' `elem` str = Alternative . map parseRule . splitOn "|" $ str
                       | otherwise      = Sequence . map read . words $ str
 
-check :: Rules -> Rule -> String -> (Bool, String)
-check _ (Leaf _) [] = (False, "")
-check _ (Leaf c) cs = bool (False, cs) (True, tail cs) . (c == ) . head $ cs
-
-check rules (Alternative l r) cs = if leftValid || rightValid
-                                    then bool right left leftValid
-                                    else (False, cs)
+check :: Rules -> Rule -> String -> [String]
+check _     (Leaf _)         [] = []
+check _     (Leaf c)         cs = bool [] [tail cs] . (c == ) . head $ cs
+check rules (Alternative rs) cs = rs >>= (\rule -> check rules rule cs)
+check rules (Sequence is)    cs = foldl checkRule [cs] . map (rules ! ) $ is
     where
-        left@(leftValid, _)   = check rules l cs
-        right@(rightValid, _) = check rules r cs
-
-check rules (Sequence is) cs = foldl checkRule (True, cs) . map (rules ! ) $ is
-    where
-        checkRule (False, cs') _   = (False, cs')
-        checkRule (True, cs') rule = check rules rule cs'
+        checkRule css rule = css >>= check rules rule
 
 solvePart1 :: IntMap Rule -> [String] -> Int
-solvePart1 rules = length . filter matches0
+solvePart1 rules = length . filter (any null . check rules (rules ! 0))
+
+solvePart2 :: IntMap Rule -> [String] -> Int
+solvePart2 rules = solvePart1 rules'
     where
-        matches0 = (\(b, rs) -> b && null rs) . check rules (rules ! 0)
+        new8   = Alternative [Sequence [42], Sequence [42, 8]]
+        new11  = Alternative [Sequence [42, 31], Sequence [42, 11, 31]]
+        rules' = M.insertWith const 8 new8
+               . M.insertWith const 11 new11 $ rules
 
 main :: IO ()
 main = do
     (rules, messages) <- parseInput <$> readFile "./input/19_day.txt"
     putStrLn $ "Part 1: " ++ (show . solvePart1 rules $ messages)
+    putStrLn $ "Part 2: " ++ (show . solvePart2 rules $ messages)
