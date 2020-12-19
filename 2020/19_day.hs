b08bacb6a5f76dd596c28831796b0407ec4c65fa
Michal Mrena <mrena.michal@gmail.com>
2020-12-19 17:53:48 +0100

    Day 19 part 1.

--- 2020/19_day.hs
+++ 2020/19_day.hs
@@ -1,13 +1,12 @@
 import Data.Bool
-import Data.List
 import Data.List.Split
 import Data.IntMap (IntMap, (!))
 import qualified Data.IntMap as M
 
 type RuleId = Int
 data Rule   = Leaf Char
-            | Inner [RuleId]
-            | InnerAlt Rule Rule deriving Show
+            | Sequence [RuleId]
+            | Alternative Rule Rule deriving Show
 type Rules = IntMap Rule
 
 parseInput :: String -> (Rules, [String])
@@ -19,28 +18,31 @@ parseInput input = (M.fromList . map parseRuleLine $ rulesStr, messages)
                 [i, rest] = splitOn ":" line
 
         parseRule str | '"' `elem` str = Leaf (str !! 1)
-                      | '|' `elem` str = (\[l, r] -> InnerAlt (parseRule l) (parseRule r)) . splitOn "|" $ str
-                      | otherwise      = Inner . map read . words $ str
+                      | '|' `elem` str = (\[l, r] -> Alternative (parseRule l) (parseRule r)) . splitOn "|" $ str
+                      | otherwise      = Sequence . map read . words $ str
 
-eat :: Char -> String -> String
-eat c cs = bool cs (tail cs) ((== c) . head $ cs)
+check :: Rules -> Rule -> String -> (Bool, String)
+check _ (Leaf _) [] = (False, "")
+check _ (Leaf c) cs = bool (False, cs) (True, tail cs) . (c == ) . head $ cs
 
-check :: Rules -> Rule -> String -> [String]
-check _     (Leaf c)       rest = pure . eat c $ rest
-check rules (InnerAlt l r) rest = nub $ (check rules l rest) ++ (check rules r rest)
-check rules (Inner is)     rest = nub . foldl checkRule [rest] $ innerRules
+check rules (Alternative l r) cs = if leftValid || rightValid
+                                    then bool right left leftValid
+                                    else (False, cs)
     where
-        checkRule  = (\rests rule -> rests >>= check rules rule)
-        innerRules = map (rules ! ) is
+        left@(leftValid, _)   = check rules l cs
+        right@(rightValid, _) = check rules r cs
+
+check rules (Sequence is) cs = foldl checkRule (True, cs) . map (rules ! ) $ is
+    where
+        checkRule (False, cs') _   = (False, cs')
+        checkRule (True, cs') rule = check rules rule cs'
 
 solvePart1 :: IntMap Rule -> [String] -> Int
 solvePart1 rules = length . filter matches0
     where
-        matches0 m = [] `elem` check rules (rules ! 0) m
+        matches0 = (\(b, rs) -> b && null rs) . check rules (rules ! 0)
 
-solve :: IO ()
-solve = do
+main :: IO ()
+main = do
     (rules, messages) <- parseInput <$> readFile "./input/19_day.txt"
     putStrLn $ "Part 1: " ++ (show . solvePart1 rules $ messages)
-
-main = solve
