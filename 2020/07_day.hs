095c8d6d0379a54e427e02012709940550151ebc
Michal Mrena <mrena.michal@gmail.com>
2020-12-29 15:28:49 +0100

    Day 7.

--- 2020/07_day.hs
+++ 2020/07_day.hs
@@ -2,39 +2,40 @@ import Data.Map (Map, (!))
 import qualified Data.Map as M
 
 type BagName = String
-type Bag = [(Int, BagName)]
-type BagMap = Map BagName Bag
+type Bag     = [(Int, BagName)]
+type BagMap  = Map BagName Bag
 
-parseBag :: BagMap -> String -> BagMap
-parseBag bagMap str = insertBag bagMap $ words $ filter (/= ',') $ init str
+parseInput :: String -> BagMap
+parseInput = foldl parseBag M.empty . lines
     where
-        insertBag bm (hue : color : _ : _ : rest) = M.insert (hue ++ color) (parseRest rest) bm
-        insertBag _ _ = error "not good"
-        parseRest [] = []
-        parseRest ["no", "other", "bags"] = []
-        parseRest (count : hue : color : _ : rest) = (read count, hue ++ color) : (parseRest rest)
-        parseRest _ = error "not good"
+        parseBag bagMap str = insertBag bagMap $ words $ filter (/= ',') $ init str
+            where
+                insertBag bm (hue : color : _ : _ : rest) = M.insert (hue ++ color) (parseRest rest) bm
+                insertBag _ _ = error "not good"
+                parseRest [] = []
+                parseRest ["no", "other", "bags"] = []
+                parseRest (count : hue : color : _ : rest) = (read count, hue ++ color) : (parseRest rest)
+                parseRest _ = error "not good"
 
-parseInput :: IO BagMap
-parseInput = do
-    bagLines <- lines <$> readFile "./input/07_day.txt"
-    return $ foldl parseBag M.empty bagLines
+solvePart1 :: BagMap -> Int
+solvePart1 bm = length . filter (hasGoldBag bm) $ (M.keys bm)
+    where
+        hasGoldBag bagMap current = ("shinygold" `elem` subBags) || (any (hasGoldBag bagMap) subBags)
+            where
+                subBags = map snd $ bagMap ! current
 
-canContainGoldBag :: BagMap -> BagName -> Bool
-canContainGoldBag bagMap current = let subBags = map snd $ bagMap ! current
-                                   in ("shinygold" `elem` subBags) || (any (canContainGoldBag bagMap) subBags)
+solvePart2 :: BagMap -> Int
+solvePart2 bm = tbc "shinygold"
+    where
+        tbc bn = let subBags      = bm ! bn
+                     subBagCounts = map fst subBags
+                     subBagNames  = map snd subBags
+                 in if null subBags
+                     then 1
+                     else (+ 1) . sum . zipWith (*) subBagCounts . map tbc $ subBagNames
 
-totalBackCount :: BagMap -> BagName -> Int
-totalBackCount bagMap bagName = let subBags      = bagMap ! bagName
-                                    subBagCounts = map fst subBags
-                                    subBagNames  = map snd subBags
-                                in if (null subBags)
-                                       then 1
-                                       else (+ 1) . sum . zipWith (*) subBagCounts . map (totalBackCount bagMap) $ subBagNames
-
-solve :: IO ()
-solve = do
-    bagMap <- parseInput
-    let validBags = filter (canContainGoldBag bagMap) (M.keys bagMap)
-    putStrLn $ "Part 1: " ++ (show . length $ validBags)
-    putStrLn $ "Part 2: " ++ (show . totalBackCount bagMap $ "shinygold")
+main :: IO ()
+main = do
+    input <- parseInput <$> readFile "./input/07_day.txt"
+    putStrLn $ "Part 1: " ++ (show . solvePart1 $ input)
+    putStrLn $ "Part 2: " ++ (show . solvePart2 $ input)
