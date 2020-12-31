051d51c17806dc772c10b384a34845556bcf08f9
Michal Mrena <mrena.michal@gmail.com>
2020-12-31 12:03:20 +0100

    2018 Day 1.

---
+++ 2018/01_day.hs
@@ -0,0 +1,21 @@
+import qualified Data.IntSet as S
+
+parseInput :: String -> [Int]
+parseInput = map read . lines . filter (/= '+')
+
+solvePart1 :: [Int] -> Int
+solvePart1 = sum
+
+solvePart2 :: [Int] -> Int
+solvePart2 = loop S.empty . scanl (+) 0 . cycle
+    where
+        loop fs (x : xs)
+            | x `S.member` fs = x
+            | otherwise       = loop (S.insert x fs) xs
+        loop _ _ = undefined
+
+main :: IO ()
+main = do
+    input <- parseInput <$> readFile "./input/01_day.txt"
+    putStrLn $ "Part 1: " ++ (show . solvePart1 $ input)
+    putStrLn $ "Part 2: " ++ (show . solvePart2 $ input)
