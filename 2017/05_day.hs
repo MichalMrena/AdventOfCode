443f5e2705bc73d7a0794094860866a06c635ad2
Michal Mrena <mrena.michal@gmail.com>
2021-09-20 16:28:17 +0200

    2017 Day 5.

--- 2017/05_day.hs
+++ 2017/05_day.hs
@@ -1,20 +1,30 @@
 import Control.Monad.ST ( ST, runST )
-import Data.STRef ( newSTRef, writeSTRef )
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as VM
+import qualified Data.Array.Base as VM
 
-part1 :: Int
-part1 = runST $ do
-    xs <- V.thaw $ V.fromList ([1..10] :: [Int])
-    res <- newSTRef 0
+jump :: (Int -> Int) -> VM.MVector s Int -> Int -> Int -> ST s Int
+jump off xs n i = do
+    step <- VM.read xs i
+    let step' = off step
+    VM.write xs i step'
+    let i' = i + step
+    if i' < 0 || i' >= VM.length xs
+        then return n
+        else jump off xs (n + 1) i'
 
-    jmp xs res 0
+part1 :: [Int] -> Int
+part1 js = runST $ do
+    xs <- V.unsafeThaw . V.fromList $ js
+    jump succ xs 1 0
 
-    return 1
-    where
-        -- TODO do this using lambda?
-        jmp vec r n = do
-            if n == 10
-                then do VM.write vec 0 1
-                        jmp vec r (n + 1)
-                else writeSTRef r n
+part2 :: [Int] -> Int
+part2 js = runST $ do
+    xs <- V.unsafeThaw . V.fromList $ js
+    jump (\o -> if o >= 3 then pred o else succ o) xs 1 0
+
+main :: IO ()
+main = do
+    xs <- map read . lines <$> readFile "./input/05_day.txt"
+    putStrLn $ "Part 1: " ++ (show . part1 $ xs)
+    putStrLn $ "Part 2: " ++ (show . part2 $ xs)
