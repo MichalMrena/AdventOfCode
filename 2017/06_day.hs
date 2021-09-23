13d1da92017232878c8cdf86b91b1090c5633bac
Michal Mrena <mrena.michal@gmail.com>
2021-09-23 15:32:07 +0200

    2017 Day 6.

--- 2017/06_day.hs
+++ 2017/06_day.hs
@@ -1,4 +1,5 @@
 import qualified Data.Set as S
+import qualified Data.Map as M
 import           Data.Vector.Unboxed ((!), (//))
 import qualified Data.Vector.Unboxed as V
 
@@ -28,7 +29,20 @@ part1 banks = go (S.singleton banks) banks 1
                 xs'   = realloc xs
                 prev' = S.insert xs' prev
 
+part2 :: V.Vector Int -> Int
+part2 banks = go (M.singleton banks 0) banks 1
+    where
+        xss = iterate realloc banks
+
+        go prev xs n = case M.lookup xs' prev of
+                           (Just i) -> n - i
+                           Nothing  -> go prev' xs' (n + 1)
+            where
+                xs'   = realloc xs
+                prev' = M.insert xs' n prev
+
 main :: IO ()
 main = do
     xs <- V.fromList . map read . words <$> readFile "./input/06_day.txt"
     putStrLn $ "Part 1: " ++ (show . part1 $ xs)
+    putStrLn $ "Part 2: " ++ (show . part2 $ xs)
