ef1c05ebb21e049fb381811a562378da70b0160a
Michal Mrena <mrena.michal@gmail.com>
2021-12-03 10:09:40 +0100

    Day 3, part 2, needs refactor.

--- 2021/src/Day03.hs
+++ 2021/src/Day03.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE TupleSections #-}
 module Day03 where
 
 import Data.Char ( ord )
 import Data.List ( transpose )
+
+bitFreq :: [Char] -> (Int, Int)
+bitFreq = foldl incFreq (0, 0)
+  where incFreq (z, o) '0' = (z + 1, o)
+        incFreq (z, o) '1' = (z, o + 1)
 
 part1 :: [[Char]] -> Int
 part1 nums = gamma * epsilon
@@ -14,17 +20,28 @@ part1 nums = gamma * epsilon
         epsilon     = sum $ zipWith (*) epsilonBits (iterate (*2) 1)
 
 part2 :: [[Char]] -> Int
-part2 nums = 0
-  where 
-        go :: ([Char] -> Char) -> [(String, String)] -> String
-        go sigBit [(ls, rs)] = reverse rs ++ ls
-        go sigBit ns         = go sigBit (filter (\(_, r : rs) -> r == bit) ns)
-          where bit = sigBit $ map (head . snd) ns
+part2 nums = oxygen * co2
+  where bitOxygen bs = let (z, o) = bitFreq bs
+                       in if o >= z then '1' else '0'
+        bitCo2 bs = let (z, o) = bitFreq bs
+                    in if z <= o then '0' else '1'
+
+        oxygenBits = go bitOxygen (map ([],) nums)
+        co2Bits    = go bitCo2 (map ([],) nums)
+
+        oxygen = sum $ zipWith (*) oxygenBits (iterate (*2) 1)
+        co2    = sum $ zipWith (*) co2Bits (iterate (*2) 1)
+
+        go :: ([Char] -> Char) -> [(String, String)] -> [Int]
+        go sigBit [(ls, rs)] = map (subtract (ord '0') . ord) $ reverse rs ++ ls
+        go sigBit ns         = go sigBit ns'
+          where bit  = sigBit $ map (head . snd) ns
+                rest = filter (\(_, r : rs) -> r == bit) ns
+                ns'  = map (\(ls, r : rs) -> (r : ls, rs)) rest
 
 solveDay :: IO ()
 solveDay = do
   numbers <- lines <$> readFile "input/03_day.txt"
   putStrLn $ "Part 1: " ++ (show . part1 $ numbers)
-  -- putStrLn $ "Part 2: " ++ (show . part2 $ numbers)
-  print (part2 numbers)
+  putStrLn $ "Part 2: " ++ (show . part2 $ numbers)
   putStrLn "done"
