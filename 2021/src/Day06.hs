16358e82d7153cabafcfe889628f87e02a43a3cf
Michal Mrena <mrena.michal@gmail.com>
2022-05-23 08:19:14 +0200

    Refactoring.

--- 2021/src/Day06.hs
+++ 2021/src/Day06.hs
@@ -4,12 +4,15 @@ module Day06 where
 import qualified Text.Parsec as P
 
 count :: Int -> Int
-count days = 0
+count days | days < 0  = 0
+           | otherwise = 1 + sum (map count $ takeWhile (>0) children)
+  where children = (days - 8) : [(days - 8) - 7 * i | i <- [1 ..]]
 
 solveDay :: IO ()
 solveDay = do
   ns <- readNums <$> readFile "input/06_day.txt" :: IO [Int]
   print ns
+  print (sum $ map (count  . (19 -)) ns)
   putStrLn "done"
   where readNums str = case P.parse nums "" str of
                          (Right ns) -> ns
