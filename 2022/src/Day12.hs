9027a1c578ec3f63f142b8a5f843944349482274
Michal Mrena <mrena.michal@gmail.com>
2022-12-20 16:08:55 +0100

    Day 12 part 2.

--- 2022/src/Day12.hs
+++ 2022/src/Day12.hs
@@ -1,26 +1,28 @@
 module Day12 where
 
+import           Data.Maybe ( fromMaybe )
+import           Data.Char ( ord )
 import           Data.List ( elemIndex, foldl' )
 import qualified Data.Map.Strict as Mp
 import           Data.Map.Strict ( (!) )
 import qualified Data.Matrix as Mt
 import           Data.Maybe ( isNothing, fromMaybe )
 import qualified Data.Sequence as S
-import           Data.Sequence ( (|>), Seq (Empty, (:<|), (:|>)), (><) )
+import           Data.Sequence ( Seq (Empty, (:<|)), (><) )
 
 type Position = (Int, Int)
+type Heights  = Mt.Matrix Char
 type Canyon   = (Position, Position, Mt.Matrix Char)
+type TestEdge = (Char -> Char -> Bool)
 
-part1 :: Canyon -> Int
-part1 (src, dst, heights) = go (Mp.singleton src 0) (S.singleton src)
-  where
-        nrow = Mt.nrows heights
-        ncol = Mt.ncols heights
+pointtoall :: TestEdge -> Position -> Heights -> [((Int, Int), Int)]
+pointtoall test src heights = Mp.toList finaldists
+  where nrow       = Mt.nrows heights
+        ncol       = Mt.ncols heights
+        finaldists = go (Mp.singleton src 0) (S.singleton src)
 
-        --go :: Mp.Map (Int, Int) Int -> S.Seq (Int, Int) -> Int
-        go dists (p :<| ps)
-          | p == dst  = d
-          | otherwise = go dists' ps'
+        go dists Empty      = dists
+        go dists (p :<| ps) = go dists' ps'
           where
             (r, c) = p
             d      = dists ! p
@@ -31,27 +33,37 @@ part1 (src, dst, heights) = go (Mp.singleton src 0) (S.singleton src)
             cango nei@(r', c') =
               (r' >= 1 && r' <= nrow) &&
               (c' >= 1 && c' <= ncol) &&
-              ((heights Mt.! nei) <= succ (heights Mt.! p)) &&
+              (test (heights Mt.! nei) (heights Mt.! p)) &&
               (not (nei `Mp.member` dists))
 
-
-
+part1 :: Canyon -> Int
+part1 (src, dst, heights) = result
+  where
+        result       = fromMaybe undefined $ lookup dst finaldists
+        finaldists   = pointtoall test src heights
+        test to from = to <= succ from
 
 part2 :: Canyon -> Int
-part2 = const 2
+part2 (_, dst, heights) = result
+  where
+    result       = minimum . map snd $ as
+    as           = filter ((=='a') . (heights Mt.!) . fst) $ finaldists
+    finaldists   = pointtoall test dst heights
+    test to from = (cdiff to from >= -1)
+    cdiff a b    = (ord a) - (ord b)
 
 solveDay :: IO ()
 solveDay = do
   xs@(s, d, m) <- parseInput <$> readFile "input/12_day.txt"
   putStrLn $ "Part 1: " ++ (show . part1 $ xs)
   putStrLn $ "Part 2: " ++ (show . part2 $ xs)
-  where parseInput s = (src, dst, mat)
-          where xss = lines s
-                mat = Mt.setElem 'z' dst . Mt.setElem 'a' src . Mt.fromLists $ xss
-                src = findPos xss 'S'
-                dst = findPos xss 'E'
-
-        findPos xss c = (row, 1 + fromMaybe undefined col)
-          where mbs        = map (elemIndex c) xss
-                (row, col) = head . dropWhile (isNothing . snd)
-                           . zip [1..] $ mbs
+  where
+    parseInput s = (src, dst, mat)
+      where xss = lines s
+            mat = Mt.setElem 'z' dst . Mt.setElem 'a' src . Mt.fromLists $ xss
+            src = findPos xss 'S'
+            dst = findPos xss 'E'
+    findPos xss c = (row, 1 + fromMaybe undefined col)
+      where mbs        = map (elemIndex c) xss
+            (row, col) = head . dropWhile (isNothing . snd)
+                       . zip [1..] $ mbs
